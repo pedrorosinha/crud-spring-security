a0a7607e75fbf21c03d39fcd4780c019cd4404af
pedro felipe <pedrofelipinho@gmail.com>
2024-08-13 15:11:33 -0300

    Criando p projeto do crud de uma loja focado no spring security

---
+++ projeto-spring-security/src/main/java/br/com/dbserver/projeto_spring_security/ProjetoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbserver.projeto_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringSecurityApplication.class, args);
+	}
+
+}
