38ddadf8a68ffb0e04f04f57dea28a1ea87c383a
pedro felipe <pedrofelipinho@gmail.com>
2024-08-13 16:39:48 -0300

    Ajustando os repositórios de Cliente e Produto

--- projeto-spring-security/src/main/java/br/com/dbserver/projeto_spring_security/domain/cliente/ClienteRepository.java
+++ projeto-spring-security/src/main/java/br/com/dbserver/projeto_spring_security/domain/cliente/ClienteRepository.java
@@ -1,9 +1,8 @@
 package br.com.dbserver.projeto_spring_security.domain.cliente;
 
-import br.com.dbserver.projeto_spring_security.domain.produto.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Produto, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
--- projeto-spring-security/src/main/java/br/com/dbserver/projeto_spring_security/domain/produto/ProdutoRepository.java
+++ projeto-spring-security/src/main/java/br/com/dbserver/projeto_spring_security/domain/produto/ProdutoRepository.java
@@ -1,10 +1,9 @@
 package br.com.dbserver.projeto_spring_security.domain.produto;
 
-import br.com.dbserver.projeto_spring_security.domain.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Cliente,Long> {
+public interface ProdutoRepository extends JpaRepository<Produto,Long> {
 
 }
